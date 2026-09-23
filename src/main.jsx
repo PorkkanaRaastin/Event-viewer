d631e6c55d8eba7815c75655b0df6140f29146d2
PorkkanaRaastin <rasmusthegamer1@gmail.com>
2026-09-23 12:56:27 +0300

    register working + some cool background effects

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,17 @@ import AdminLogin from '../pages/admin_login.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/Login" replace />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/AdminLogin" element={<AdminLogin />} />
-        <Route path="/UserPage" element={<UserPage />} />
-        <Route path="/AdminPage" element={<AdminPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="starsContainer">
+      <div className="starsLayer starsSmall"></div>
+      <div className="starsLayer starsMedium"></div>
+      <div className="starsLayer starsLarge"></div>
+    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/Login" replace />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/UserPage" element={<UserPage />} />
+      <Route path="/AdminPage" element={<AdminPage />} />
+    </Routes>
+  </BrowserRouter>
   </StrictMode>,
 )
